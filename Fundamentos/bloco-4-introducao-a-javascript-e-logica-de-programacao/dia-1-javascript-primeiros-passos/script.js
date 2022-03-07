604ce3292bfcef22c81aeb3d23a6a6030cb6d9c9
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-03-07 18:32:49 -0300

    Operações aritméticas básicas.

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,17 @@
+const a = 10;
+const b = 5;
+
+//soma
+console.log(a+b);
+
+//subtração
+console.log(a-b);
+
+//multiplicação
+console.log(a*b);
+
+//divisão
+console.log(a/b);
+
+//Módulo
+console.log(a%b);
