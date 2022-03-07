264cf49efc73afb8584f49b61db1e1618d71cc79
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-03-07 18:54:01 -0300

    Verifica se o número é positivo, negativo ou zero.

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,11 +1,9 @@
-const num1 = 50;
-const num2 = 40;
-const num3 = 30;
+const num1 = 0;
 
-if ((num1 > num2) && (num1 > num3)) {
-    console.log("O primeiro número é o maior entre os três.");
-} else if((num2 > num1) && (num2 > num3)) {
-    console.log("O segundo número é o maior entre os três.");
-} else {
-    console.log("O terceiro número é o maior entre os três.");
+if (num1 > 0) {
+    console.log("O número", num1, "é positivo.");
+}else if (num1 < 0) {
+    console.log("O número", num1, "é negativo.")
+}else {
+    console.log("O número é zero.")
 }
