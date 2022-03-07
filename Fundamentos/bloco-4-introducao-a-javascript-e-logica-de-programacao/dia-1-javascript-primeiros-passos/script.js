72c436f2a1338ea214f8781b047b0c0cf2857403
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-03-07 19:09:32 -0300

    Ângulos do triângulo.

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,9 +1,11 @@
-const num1 = 0;
+const ang1 = 10;
+const ang2 = 40;
+const ang3 = 70;
 
-if (num1 > 0) {
-    console.log("O número", num1, "é positivo.");
-}else if (num1 < 0) {
-    console.log("O número", num1, "é negativo.")
+if ((ang1 < 0) || (ang2 < 0) || (ang3 < 0)) {
+    console.log("False,ângulo inválido.");
+}else if(ang1+ang2+ang3 > 180) {
+    console.log("False");
 }else {
-    console.log("O número é zero.")
+    console.log("True");
 }
