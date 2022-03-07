5684f064f4f438cb002d8a1c232750d573b908a1
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-03-07 18:48:54 -0300

    Verifica qual dos três valores é o maior.

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,8 +1,11 @@
-const num1 = 10;
-const num2 = 25;
+const num1 = 50;
+const num2 = 40;
+const num3 = 30;
 
-if (num1 > num2) {
-    console.log(num1 ,"é maior do que", num2);
-}else {
-    console.log(num2, "é maior do que", num1);
+if ((num1 > num2) && (num1 > num3)) {
+    console.log("O primeiro número é o maior entre os três.");
+} else if((num2 > num1) && (num2 > num3)) {
+    console.log("O segundo número é o maior entre os três.");
+} else {
+    console.log("O terceiro número é o maior entre os três.");
 }
