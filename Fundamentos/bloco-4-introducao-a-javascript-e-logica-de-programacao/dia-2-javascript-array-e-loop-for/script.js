dfe9f4e98c3c2590476af9e1a7821201582a6ff8
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-03-08 18:31:38 -0300

    exercicio 4, media aritmetica.

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -14,3 +14,30 @@ for(let index1 = 0; index1 < numbers1.length; index1 += 1) {
 }
 
 console.log("O resultado da soma do conteúdo do array é:", somaArray); 
+
+//exercicio 3 - calcule e imprima a média aritmética dos valores contidos no array.
+let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultSoma = 0;
+let qntdIndice = 0;
+let mediaAritmetica = 0;
+
+for(let index2 = 0; index2 < numbers2.length; index2 += 1) {
+    resultSoma += numbers2[index2];
+    qntdIndice += 1;
+}
+
+mediaAritmetica = resultSoma / qntdIndice;
+
+console.log('A média aritmética do array é: ', mediaAritmetica);
+
+//exercicio 3 - gabarito course.
+let num = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for (let index = 0; index < num.length; index += 1) {
+  sum += num[index];
+}
+
+let average = sum / num.length;
+
+console.log(average);
