b4d1573400963afd1772896cd664446c90126d06
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-03-08 19:33:05 -0300

    exercicio 7, menor valor do array.

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,7 +1,7 @@
 //exercicio 1
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let index = 0; index < numbers.length; index += 1) {
+for (let index = 0; index < numbers.length; index += 1) {
     console.log(numbers[index]);
 }
 
@@ -9,11 +9,11 @@ for(let index = 0; index < numbers.length; index += 1) {
 let numbers1 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let somaArray = 0;
 
-for(let index1 = 0; index1 < numbers1.length; index1 += 1) {
+for (let index1 = 0; index1 < numbers1.length; index1 += 1) {
     somaArray += numbers1[index1];
 }
 
-console.log("O resultado da soma do conteúdo do array é:", somaArray); 
+console.log("O resultado da soma do conteúdo do array é:", somaArray);
 
 //exercicio 3 - calcule e imprima a média aritmética dos valores contidos no array.
 let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
@@ -21,7 +21,7 @@ let resultSoma = 0;
 let qntdIndice = 0;
 let mediaAritmetica = 0;
 
-for(let index2 = 0; index2 < numbers2.length; index2 += 1) {
+for (let index2 = 0; index2 < numbers2.length; index2 += 1) {
     resultSoma += numbers2[index2];
     qntdIndice += 1;
 }
@@ -35,7 +35,7 @@ let num = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 
 for (let index = 0; index < num.length; index += 1) {
-  sum += num[index];
+    sum += num[index];
 }
 
 let average = sum / num.length;
@@ -48,16 +48,16 @@ let resultSoma1 = 0;
 let qntdIndice1 = 0;
 let mediaAritmetica1 = 0;
 
-for(let index3 = 0; index3 < numbers3.length; index3 += 1) {
+for (let index3 = 0; index3 < numbers3.length; index3 += 1) {
     resultSoma1 += numbers3[index3];
     qntdIndice1 += 1;
 }
 
 mediaAritmetica1 = resultSoma1 / qntdIndice1;
 
-if(mediaAritmetica1 > 20) {
+if (mediaAritmetica1 > 20) {
     console.log('A média aritmética é maior do que 20. Média:', mediaAritmetica1);
-}else{
+} else {
     console.log('A média aritmética é menor do que 20. Média:', mediaAritmetica1);
 }
 
@@ -65,8 +65,8 @@ if(mediaAritmetica1 > 20) {
 let numbers4 = [5, 9, 3, 90, 70, 8, 100, 2, 35, 27];
 let maior = 0;//variável que armazena o maior número.
 
-for(let index4 = 0 ; index4 < numbers4.length; index4 += 1) {
-    if(numbers4[index4] > maior){//testa qual é o maior e armazena.
+for (let index4 = 0; index4 < numbers4.length; index4 += 1) {
+    if (numbers4[index4] > maior) {//testa qual é o maior e armazena.
         maior = numbers4[index4];
     }
 }
@@ -77,14 +77,28 @@ console.log('O maior número do array é:', maior);
 let numbers5 = [5, 9, 3, 90, 70, 8, 100, 2, 35, 27];
 let numbersImpar = 0;
 
-for(let index5 = 0; index5 < numbers5.length; index5 += 1) {
-    if(numbers5[index5] % 2 !== 0) {
+for (let index5 = 0; index5 < numbers5.length; index5 += 1) {
+    if (numbers5[index5] % 2 !== 0) {
         numbersImpar += 1;
     }
 }
 
 if (numbersImpar !== 0) {
-    console.log('O array contém ', numbersImpar,'números ímpares.')
-}else {
+    console.log('O array contém ', numbersImpar, 'números ímpares.')
+} else {
     console.log('Nenhum valor ímpar encontrado.');
 }
+
+//exercicio 7 - menor valor do array
+let numbers6 = [5, 9, 3, 90, 70, 8, 100, 2, 35, 27];
+// o valor da variável resulMenorValor poderia ser qualquer um, desde que fosse maior que o maior número do array numbers6. Caso atribuíssemos o valor 1 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número menor que 1 no array. Para resolver esse problema vamos iniciar a variavel com o primeiro valor do array. Dessa forma podemos pular a posição 0 e iniciar com index = 1
+
+let resulMenorValor = numbers6[0];
+
+for (let index6 = 1; index6 < numbers6.length; index6 += 1) {
+    if (numbers6[index6] < resulMenorValor) {
+        resulMenorValor = numbers6[index6];
+    }
+}
+
+console.log('O menor valor do array é o número:', resulMenorValor);
