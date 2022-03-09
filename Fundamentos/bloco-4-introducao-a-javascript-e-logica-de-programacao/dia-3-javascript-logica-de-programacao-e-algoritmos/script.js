b6c9258e50a071645290f138ccbc2d0baf5f2bd3
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-03-09 15:27:59 -0300

    exercicio - fatorial.

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,8 @@
+//exercicio 1 - fatorial do número 10
+let resultado = 1;
+
+for(let index = 2; index <= 10; index += 1) {
+    resultado *= index;
+}
+
+console.log('O fatorial do número 10 é:', resultado);
