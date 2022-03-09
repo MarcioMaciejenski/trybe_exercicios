d83f3d33c828ee44cee999564e33e2b41daf24d5
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-03-09 15:47:03 -0300

    exercicio 2 - inverter palavra.

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -6,3 +6,22 @@ for(let index = 2; index <= 10; index += 1) {
 }
 
 console.log('O fatorial do número 10 é:', resultado);
+
+//exercicio 2 - inverter palavra.
+let word = 'tryber';
+let inverseWord = word.split("").reverse().join("");//código do site horadecodar.com.br
+
+console.log(word , inverseWord);
+
+//código do site horadecodar.com.br, utilizando uma função.
+let string = 'javascript'
+
+function inverterString(str) {
+    let inverseString = '';
+    for(let i = str.length -1; i >= 0; i -= 1){
+        inverseString += str[i];
+    }
+    return inverseString
+}
+
+console.log(inverterString(string));
