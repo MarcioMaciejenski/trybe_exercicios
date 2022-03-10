4445e252e4ce318cc3d4e5ec0eada06c6784de57
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-03-10 18:34:10 -0300

    exercicio5, criado novo objeto e listado valores dos dois lado a lado.

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -6,18 +6,34 @@ let info = {
   };
 
   console.log('Seja bem-vinda', info.personagem +'.');
-
+  console.log('----------------------------------');
  //exercicio 2 - inserir nova propriedade no objeto.
+
  info.recorrente = 'sim';
 
  console.log(info);
-
+ console.log('----------------------------------');
 // exercicio 3 - for/in  mostra todas as chaves do objeto.
+
 for(let key in info){
     console.log(key);
 }
+console.log('----------------------------------');
 
 //exercicio 4 - for/in mostra valores das chaves do objeto
 for(let key1 in info) {
     console.log(info[key1]);
+}
+console.log('----------------------------------');
+
+//exercicio 5 - criar novo objeto e imprimir valores correspondente dos dois.
+let newInfo = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'sim',
+}
+
+for(let key2 in info , newInfo) {
+    console.log(info[key2],'e',newInfo[key2]);
 }
