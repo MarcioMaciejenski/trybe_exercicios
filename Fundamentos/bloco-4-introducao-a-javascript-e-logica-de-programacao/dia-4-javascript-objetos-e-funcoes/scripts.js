2d186040625445922804dbbccdd99bc63d1bc3f1
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-03-10 18:17:14 -0300

    exercicios 1 e 2, exibir mensagem e inserir propriedade no objeto

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -0,0 +1,14 @@
+//exercicio 1- imprimir mensagem de boas vindas.
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log('Seja bem-vinda', info.personagem +'.');
+
+ //exercicio 2 - inserir nova propriedade no objeto.
+ info.recorrente = 'sim';
+
+ console.log(info);
+
