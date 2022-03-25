7c0da8a167fe20d6c79270948b5c9e2e79505811
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-03-25 10:21:40 -0300

    Exercício bônus 2.

--- Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -17,6 +17,18 @@ function limpar() { // Seleciona os inputs e o textarea, depois limpa o que tive
    areaTexto.value = '';
 }
 
+function validaInputs() {
+    const textoResposta = document.querySelector('#texto-resposta').value.length;
+    const nome = document.querySelector('#nome').value.length;
+    const email = document.querySelector('#email').value.length;
+    if(textoResposta > 500 || nome < 10 || nome > 40 || email < 10 || email > 40){
+        return alert('Dados inválidos');
+    }else {
+        return alert( 'Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.' );
+    }
+}
+enviar.addEventListener('click', validaInputs);
+
 window.onload = function () { // Chama as funções depois do carregamento da página.
   const botaoEnviar = document.querySelector('#botao');
   botaoEnviar.addEventListener('click', botaoEnviar);
