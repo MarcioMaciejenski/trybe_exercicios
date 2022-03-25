c4c3b3d7bb87487590cf6c59bb606108cfd708a1
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-03-25 10:00:54 -0300

    Correção dos exercícios 3 e 4.

--- Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,6 +1,25 @@
 // Interrompa o comportamento padrão do botão submit utilizando o método preventDefault() 
+
 const enviar = document.querySelector('#botao');
 
-enviar.addEventListener('click', function(event) {
+function botaoEnviar(event) {
     event.preventDefault();
-});
+}
+
+function limpar() { // Seleciona os inputs e o textarea, depois limpa o que tiver neles.
+  const dadosFormulario = document.querySelectorAll('input');
+  const areaTexto = document.querySelector('textarea');
+  for (let index = 0; index < dadosFormulario.length; index += 1){
+      const dado = dadosFormulario[index];
+      dado.value = '';
+      dado.checked = false;
+  }
+   areaTexto.value = '';
+}
+
+window.onload = function () { // Chama as funções depois do carregamento da página.
+  const botaoEnviar = document.querySelector('#botao');
+  botaoEnviar.addEventListener('click', botaoEnviar);
+  const botaoLimpar = document.querySelector('#limpar');
+  botaoLimpar.addEventListener('click', limpar);
+};
