f8e5a0f85d48ec92c7f451f694eb5b8a197193f8
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-04-01 19:17:31 -0300

    Exercício 1 refatorado e 2.

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -12,15 +12,28 @@
 
 //   testingScope(true);
 
-  const testingScope = (escopo) => {
-      if(escopo === true) {
-          let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-          ifScope = (`${ifScope}! ótimo,fui utilizada no escopo !`);
-          console.log(ifScope);
-      }else {
-          let elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
-          console.log(elseScope);
-      }
-     // console.log(`${ifScope}! o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
-  }
-  testingScope(true);
+//   const testingScope = (escopo) => {
+//       if(escopo === true) {
+//           let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//           ifScope = (`${ifScope}! ótimo,fui utilizada no escopo !`);
+//           console.log(ifScope);
+//       }else {
+//           let elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+//           console.log(`${elseScope}`);
+//       }
+//       console.log(`${ifScope}! o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
+//   }
+//   testingScope(true);
+
+//Usando operador ternário
+let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+let elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+const testingScope = (escopo) => escopo ? `${ifScope}! ótimo,fui utilizada no escopo !` : `${elseScope}`;
+
+console.log(testingScope(true));
+  //Exercício 2
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+  oddsAndEvens.sort((a, b) => a - b);
+
+  console.log(`Array em ordem crescente ${oddsAndEvens} .`); // será necessário alterar essa linha 😉
