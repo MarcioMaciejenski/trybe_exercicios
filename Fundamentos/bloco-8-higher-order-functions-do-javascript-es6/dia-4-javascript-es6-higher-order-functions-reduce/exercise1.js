b1d93923b4e2b669835fd892f82c042bd0ba39f9
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-04-12 19:07:31 -0300

    Exercício 2.

--- Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -1,12 +1,11 @@
 // Dada uma matriz, transforme em um array.
 const arrays = [
-    ['1', '2', '3'],
-    [true],
-    [4, 5, 6],
-  ];
-  
-  const flatten = arrays.reduce((acc , itens) => acc.concat(itens),[]);
-  
-  console.log(flatten);
-    
-  
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+const flatten = arrays.reduce((acc, itens) => acc.concat(itens), []);
+//O método concat() retorna um novo array contendo todos os arrays ou valores passados como parâmetro.
+console.log(flatten);
+
