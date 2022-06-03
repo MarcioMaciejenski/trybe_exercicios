490202a427720b9a3da3ada98448fa228240c753
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-06-03 10:35:29 -0300

    Exercício

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/mocks/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/mocks/service.test.js
@@ -7,5 +7,14 @@ describe('Testes da função "randomNumber"', () => {
     expect(services.randomNumber()).toBe(10);
     expect(services.randomNumber).toHaveBeenCalled();
     expect(services.randomNumber).toHaveBeenCalledTimes(1);
-  })
+  });
+  it('Função recebe dois parâmetros e retorna a divisão do primeiro pelo segundo', () => {
+    services.randomNumber.mockImplementation((a,b) => a/b);
+    services.randomNumber(10,2);
+
+    expect(services.randomNumber).toHaveBeenCalled();
+    expect(services.randomNumber).toHaveBeenCalledTimes(2);
+    expect(services.randomNumber).toHaveBeenCalledWith(10,2);
+    expect(services.randomNumber(10,2)).toBe(5);
+  });
 })
