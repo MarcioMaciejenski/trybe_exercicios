15df91e45a3fde1e41408df1f44ae247152e5446
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-08-23 10:47:16 -0300

    feat: exercício 4.

--- Back_End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/src/app.js
+++ Back_End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const activities = require('./data/activities');
 
 const app = express();
+app.use(express.json());
+
+// Crie um endpoint do tipo GET com a rota /myActivities/:id, que possa listar uma atividade do array por id 🚀
+app.get('/myActivities/:id', (req, res) => {
+  const activity = activities.find(({ id }) => id === Number(req.params.id));
+  res.status(200).json(activity);
+});
 
 module.exports = app;
