26099e0e05e57c343cadbd2ec2cc7fe7acfd601d
Marcio Maciejenski <marciomaciejenski@yahoo.com.br>
2022-08-25 13:53:53 -0300

    feat: Exercíxio 2 feito.

--- Back_End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-middlewares/src/app.js
+++ Back_End/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-middlewares/src/app.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const auth = require('./middlewares/auth');
 const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
 const validateDescription = require('./middlewares/validateDescription');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
 const validateRating = require('./middlewares/validateRating');
 const validateDifficulty = require('./middlewares/validateDifficulty');
+const generateToken = require('./utils/generateToken');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/activities',
+  auth,
   validateName,
   validatePrice,
   validateDescription,
@@ -19,6 +22,17 @@ app.post('/activities',
   validateDifficulty,
   (_req, res) => {
   res.status(201).json({ message: 'Atividade registrada com sucesso!' });
+});
+
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, phone } = req.body;
+  if (!email || !password || !firstName || !phone) {
+    return res.status(401).json({ message: 'Campos ausentes' });
+  }
+
+  const token = generateToken();
+
+  res.status(201).json({ token });
 });
 
 module.exports = app;
